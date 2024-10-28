1318d419c2ca0a34fef12b131fd6990b17c308de
barassulin <148526209+barassulin@users.noreply.github.com>
2024-10-29 01:19:37 +0200

    Update Client.py

--- Client.py
+++ Client.py
@@ -43,42 +43,56 @@ def handle_connection(start_number, end_number):
 
 def main():
     try:
+        global found
         client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         client_socket.connect((SERVER_IP, SERVER_PORT))
         client_socket.settimeout(20)
         # send how many cores
         cores = os.cpu_count()
         client_socket.send(protocol.send_protocol(cores))        # recv start and jumps
-        data = client_socket.recv(1).decode()
-        print("rec")
-        if data == "":
-            data = None
-        else:
-            data = protocol.recv_protocol(client_socket, data)
-            print(data)
-            print("data")
-            data2 = data.split('/', 2)
-            start_number = data2[0]
-            if start_number.isnumeric():
-                start_number = int(start_number)
-            print("data2")
-            print(data2)
-            jump = data2[1]
-            if jump.isnumeric():
-                jump = int(jump)
-            global stringi
-            stringi = data2[2]
-            end = start_number + jump * cores
-            end_number = start_number + jump
-            "thread"
-            while not found and start_number != end:
-                thread = Thread(target=handle_connection,
-                                args=(start_number, end_number))
-                thread.start()
-                stat_number = end_number
-                end_number = end_number+jump
-            client_socket.send(protocol.send_protocol(f"{found}/{found_number}".encode()))
-            print("sent")
+        while not found:
+            data = client_socket.recv(1).decode()
+            print("rec")
+            if data == "":
+                data = None
+                found = True
+
+            else:
+                data = protocol.recv_protocol(client_socket, data)
+                print(data)
+                print("data")
+                data2 = data.split('/', 2)
+                start_number = data2[0]
+                if start_number.isnumeric():
+                    start_number = int(start_number)
+                print("data2")
+                print(data2)
+                jump = data2[1]
+                if jump.isnumeric():
+                    jump = int(jump)
+                global stringi
+                stringi = data2[2]
+                end = start_number + jump * cores
+                print("cores")
+                print(cores)
+                print("end")
+                print(end)
+
+                end_number = start_number + jump
+                "thread"
+                while not found and start_number != end:
+                    thread = Thread(target=handle_connection,
+                                    args=(start_number, end_number))
+                    print(start_number)
+                    print("start_number")
+                    print(end_number)
+                    print("end_number")
+
+                    thread.start()
+                    start_number = end_number
+                    end_number = end_number+jump
+                client_socket.send(protocol.send_protocol(f"{found}/{found_number}".encode()))
+                print("sent")
 
     finally:
         client_socket.close()
